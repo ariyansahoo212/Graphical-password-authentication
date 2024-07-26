4024a4cc3380c54699cbfa5206023473f7c45ff8
Ariyan212 <ariyansahoo212@gmail.com>
2024-07-26 20:31:20 +0530

    Update config.js

--- client/src/static/config.js
+++ client/src/static/config.js
@@ -1,6 +1,6 @@
 export const api = {
     // url: "https://graphical-pass-backend.vercel.app"
-    url: process.env.NODE_ENV =="development"? "http://localhost:5000":"https://vercel.com/ariyans-projects-0c0ef2dd/graphical-password-authentication-backend/FecscbcR6WpeAtuHvGsQqiVfCU29"
+    url: process.env.NODE_ENV =="development"? "http://localhost:5000":"https://graphical-password-authentication-1bln.onrender.com"
     //url:"https://crazy-fish-tights.cyclic.app"
     // url: "https://graphical-auth-server.onrender.com"
 }
