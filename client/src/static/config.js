85ee7ee446637e2dd3c4f257ae66fc6436ddbb03
Ariyan212 <ariyansahoo212@gmail.com>
2024-07-26 19:22:26 +0530

    Add files via upload

---
+++ client/src/static/config.js
@@ -0,0 +1,6 @@
+export const api = {
+    // url: "https://graphical-pass-backend.vercel.app"
+    url: process.env.NODE_ENV =="development"? "http://localhost:5000":"https://vercel.com/ariyans-projects-0c0ef2dd/graphical-password-authentication-backend/FecscbcR6WpeAtuHvGsQqiVfCU29"
+    //url:"https://crazy-fish-tights.cyclic.app"
+    // url: "https://graphical-auth-server.onrender.com"
+}
